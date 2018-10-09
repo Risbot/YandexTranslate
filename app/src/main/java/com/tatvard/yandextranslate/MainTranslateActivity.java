1347882627fe30e46a3c1fe05d49be2a60e17685
Tatev <tatevikvardanyan21@gmail.com>
2018-10-10 00:41:55 +0400

    Splash screen// main translater

--- app/src/main/java/com/tatvard/yandextranslate/MainTranslateActivity.java
+++ app/src/main/java/com/tatvard/yandextranslate/MainTranslateActivity.java
@@ -3,11 +3,13 @@ package com.tatvard.yandextranslate;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SplashScreen extends AppCompatActivity {
+public class MainTranslateActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
+        setContentView(R.layout.activity_main_translate);
     }
 }
